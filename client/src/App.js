eab6dbdc998a577ec391a121acab48a440d2cfe1
rpparede <rpparede@uwaterloo.ca>
2021-01-13 01:19:02 -0800

    Created components to call api

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,162 @@
-import React, { useState, useEffect } from 'react';
+import React, { Component } from "react";
+import { Switch, Route, Link, BrowserRouter } from "react-router-dom";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+
+import AuthService from "./services/auth.service";
+
+import Login from "./components/login.component";
+import Register from "./components/register.component";
+import Home from "./components/home.component";
+import Profile from "./components/profile.component";
+import BoardUser from "./components/board-user.component";
+import BoardModerator from "./components/board-moderator.component";
+import BoardAdmin from "./components/board-admin.component";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.logOut = this.logOut.bind(this);
+
+    this.state = {
+      showModeratorBoard: false,
+      showAdminBoard: false,
+      currentUser: undefined,
+    };
+  }
+
+  componentDidMount() {
+    const user = AuthService.getCurrentUser();
+
+    if (user) {
+      this.setState({
+        currentUser: user,
+        showModeratorBoard: user.roles.includes("ROLE_MODERATOR"),
+        showAdminBoard: user.roles.includes("ROLE_ADMIN"),
+      });
+    }
+  }
+
+  logOut() {
+    AuthService.logout();
+  }
+
+  render() {
+    const { currentUser, showModeratorBoard, showAdminBoard } = this.state;
+
+    return (
+      <BrowserRouter>
+        <div>
+          <nav className="navbar navbar-expand navbar-dark bg-dark">
+            <Link to={"/"} className="navbar-brand">
+              bezKoder
+          </Link>
+            <div className="navbar-nav mr-auto">
+              <li className="nav-item">
+                <Link to={"/home"} className="nav-link">
+                  Home
+              </Link>
+              </li>
+
+              {showModeratorBoard && (
+                <li className="nav-item">
+                  <Link to={"/mod"} className="nav-link">
+                    Moderator Board
+                </Link>
+                </li>
+              )}
+
+              {showAdminBoard && (
+                <li className="nav-item">
+                  <Link to={"/admin"} className="nav-link">
+                    Admin Board
+                </Link>
+                </li>
+              )}
+
+              {currentUser && (
+                <li className="nav-item">
+                  <Link to={"/user"} className="nav-link">
+                    User
+                </Link>
+                </li>
+              )}
+            </div>
+
+            {currentUser ? (
+              <div className="navbar-nav ml-auto">
+                <li className="nav-item">
+                  <Link to={"/profile"} className="nav-link">
+                    {currentUser.username}
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <a href="/login" className="nav-link" onClick={this.logOut}>
+                    LogOut
+                </a>
+                </li>
+              </div>
+            ) : (
+                <div className="navbar-nav ml-auto">
+                  <li className="nav-item">
+                    <Link to={"/login"} className="nav-link">
+                      Login
+                </Link>
+                  </li>
+
+                  <li className="nav-item">
+                    <Link to={"/register"} className="nav-link">
+                      Sign Up
+                </Link>
+                  </li>
+                </div>
+              )}
+          </nav>
+
+          <div className="container mt-3">
+
+            <Switch>
+              <Route exact path={["/", "/home"]} component={Home} />
+              <Route exact path="/login" component={Login} />
+              <Route exact path="/register" component={props => <Register />} />
+              <Route exact path="/profile" component={props => <Profile />} />
+              <Route path="/user" component={props => <BoardUser />} />
+              <Route path="/mod" component={props => <BoardModerator />} />
+              <Route path="/admin" component={props => <BoardAdmin />} />
+            </Switch>
+
+          </div>
+        </div>
+      </BrowserRouter>
+    );
+  }
+}
+
+export default App;
+
+
+
+/*
+
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/** import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './App.css';
 import {
@@ -11,15 +169,15 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
 import NavDropdown from 'react-bootstrap/Navbar'
-import Home from './Components/Home'
-import NewPost from './Components/NewPost'
+import Home from './components/Home'
+import NewPost from './components/NewPost'
 function App() {
   const [state, setState] = useState([])
   useEffect(() => {
     console.log("fetching")
     /*fetch("http://localhost:3080/")
       .then(res => res.json())
-      .then(val => setState(val.test1))*/
+      .then(val => setState(val.test1))
     axios.get(`http://localhost:3080/`)
       .then(res => {
         setState([JSON.stringify(res.data)]);
@@ -61,7 +219,10 @@ function App() {
             <NewPost />
           </Route>
           <Route path="/myPosts">
-            <Dashboard />
+            <MyPosts />
+          </Route>
+          <Route path="/login">
+            <Login />
           </Route>
         </Switch>
       </div>
@@ -86,3 +247,5 @@ function Dashboard() {
   );
 }
 export default App;
+**/
+
