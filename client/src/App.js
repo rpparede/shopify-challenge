6b9e17a831ed2683ff87a1358fabe88940b3b1ca
rpparede <rpparede@uwaterloo.ca>
2021-01-12 18:54:17 -0800

    Added axios to client

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,18 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
-  const [state, setState] = useState("")
+  const [state, setState] = useState([])
   useEffect(() => {
     console.log("fetching")
-    fetch("http://localhost:3080/")
+    /*fetch("http://localhost:3080/")
       .then(res => res.json())
-      .then(val => setState(val.test1))
+      .then(val => setState(val.test1))*/
+    axios.get(`http://localhost:3080/`)
+      .then(res => {
+        setState([JSON.stringify(res.data)]);
+      })
   }, [])
   return (
     <div className="App">
